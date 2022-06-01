a4ccc37b8da5fe3e1924c1600a54ad537a56fa15
Sam Pan <59855219@qq.com>
2022-06-01 20:48:29 +0800

    2022.6.1提交

---
+++ src/components/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="example">{{ msg }}</div>
+  <el-button type="primary" @click="onSubmit">Primary</el-button>
+   
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+let msg = "login page!"
+const onSubmit = ()=>{
+  router.push('/home')
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,82 @@
+<template>
+   <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="2"
+          text-color="#fff" @open="handleOpen" @close="handleClose">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon>
+                <location />
+              </el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item one</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon>
+              <icon-menu />
+            </el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon>
+              <document />
+            </el-icon>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <el-row class="mb-4">
+            <el-button @click="onSubmit">跳转页面</el-button>
+            <el-button type="primary">Primary</el-button>
+            <el-button type="success">Success</el-button>
+            <el-button type="info">Info</el-button>
+            <el-button type="warning">Warning</el-button>
+            <el-button type="danger">Danger</el-button>
+            <el-button>中文</el-button>
+          </el-row>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: 'main'
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$router.push('/home/hello')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
