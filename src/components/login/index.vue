c8965145f5f693513cb9c2337ec03411f42b2769
Sam Pan <59855219@qq.com>
2022-06-10 19:00:21 +0800

    Squashed commit of the following:
    commit a4ccc37b8da5fe3e1924c1600a54ad537a56fa15
    Author: Sam Pan <59855219@qq.com>
    Date:   Wed Jun 1 20:48:29 2022 +0800
    
        2022.6.1提交
    
    commit c0617e644670d387d073f1091d5573d4d6066c8a
    Author: SamPan66 <7321540@qq.com>
    Date:   Sat May 28 21:20:27 2022 +0800
    
        Initial commit
    
    # Conflicts:
    #	index.html
    #	public/favicon.ico
    #	src/components/login/index.vue
    #	src/components/main.vue
    #	src/main.ts
    #	src/router/index.ts
    #	tsconfig.json
    #	yarn.lock

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,8 +1,14 @@
 <template>
+<<<<<<< HEAD
 <div class="login-bg">
   <div class="example">{{ msg }}</div>
   <el-button type="primary" @click="onSubmit">Primary</el-button>
  </div>
+=======
+  <div class="example">{{ msg }}</div>
+  <el-button type="primary" @click="onSubmit">Primary</el-button>
+   
+>>>>>>> master
 </template>
 
 <script lang="ts" setup>
@@ -10,11 +16,17 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 let msg = "login page!"
 const onSubmit = ()=>{
+<<<<<<< HEAD
   console.log(router)
+=======
+>>>>>>> master
   router.push('/home')
 }
 </script>
 
 <style>
+<<<<<<< HEAD
 .login-bg{ width: 100%; height: 100%; background: url(../../assets/login_bg.jpg) no-repeat top center;background-size:cover;}
+=======
+>>>>>>> master
 </style>
